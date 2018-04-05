4f5d227c20085a8601fbb508f4bb105b4ddaa627
david <david@cninone.com>
2018-04-05 13:07:37 +0800

    progressing

--- t.js
+++ t.js
@@ -1,7 +1,21 @@
 const dgram = require('dgram');
 const NodeRSA = require('node-rsa');
 const _ = require('lodash');
+const loki = require('lokijs')
+const lfsa = require('lokijs/src/loki-fs-structured-adapter');
 const message = Buffer.from('\x5100000000\x00');
+
+const key = new NodeRSA();
+var adapter = new lfsa();
+const db = new loki('freeland.db', { 
+    adapter : adapter,
+    autoload: true,
+    autoloadCallback : databaseInitialize,
+    autosave: true, 
+    autosaveInterval: 4000
+  })
+const users = db.addCollection('users');
+
 const client = dgram.createSocket('udp4');
 client.send(message, 1979, 'localhost', (err) => {
 //   client.close();
